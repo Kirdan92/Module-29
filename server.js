53c1068bebe18ea00b724d3f597607d837c7e2cb
Pawel Butra <pawel.butra@onet.pl>
2019-03-30 11:11:39 +0100

    add express

--- server.js
+++ server.js
@@ -155,3 +155,13 @@ Promise.all([kenny.save(), mark.save(), benny.save()])
     .then(findKennyAndDelete)
     .then(findBennyAndRemove)
     .catch(console.log.bind(console))
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)
